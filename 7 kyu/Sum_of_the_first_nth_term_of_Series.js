bae39402e99c3dd976d70c930b2e31844b802e62
Alex Deatherage <alexdeatherage4829@gmail.com>
2021-05-16 13:03:16 -0700

    add comment

--- 7 kyu/Sum_of_the_first_nth_term_of_Series.js
+++ 7 kyu/Sum_of_the_first_nth_term_of_Series.js
@@ -5,7 +5,7 @@
 // Rules:
 // You need to round the answer to 2 decimal places and return it as String.
 
-// If the given value is 0 then it should return 0.00
+// If the given value is 0 then it should return 0.00.
 
 // You will only be given Natural Numbers as arguments.
 
